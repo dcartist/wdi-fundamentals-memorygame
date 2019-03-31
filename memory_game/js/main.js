6d9acb38e9f36db749a08813718585afd3518636
dcartist <Pbannerman13@gmail.com>
2019-03-30 20:48:51 -0400

    created game logic_1

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,5 +3,5 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
-console.log("User flipped" + cardThree);
+console.log("User flipped" + cardFour);
 console.log("User flipped" + cardOne);
