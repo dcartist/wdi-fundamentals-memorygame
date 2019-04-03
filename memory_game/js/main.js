2a3b00790a11c6c619caff651b5291acc1048fda
dcartist <Pbannerman13@gmail.com>
2019-04-02 20:22:09 -0400

    first JS edits

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,33 @@
 var cards =["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped "+ cardsInPlay[0]);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped "+ cardsInPlay[1]);
-if (cardsInPlay.length == 2){
-	if (cardsInPlay[0] == cardsInPlay[1]){
-		alert("You found a match!");
+
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	  console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
+	  console.log("Sorry, try again.");
 	}
-} else {
-	console.log('blank')
 }
+
+var flipCard = function(cardId){
+//beginning of code pasted
+	if (cardsInPlay.length == 2){
+		if (cardsInPlay[0] == cardsInPlay[1]){
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
+	} else {
+		console.log('blank');
+	}
+//end of code pasted 
+console.log("User flipped" + cards[cardId])
+cardsInPlay.push(cardId);
+checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
