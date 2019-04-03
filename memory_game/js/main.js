7fe085d4eafc8e530aef3b86fc362e034c760638
dcartist <Pbannerman13@gmail.com>
2019-04-03 18:27:02 -0400

    added object to main js
    added obj to main js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,20 @@
-var cards =["queen","queen","king","king"];
+var cards =[
+{rank:"queen",
+suit:"hearts",
+cardImage:"images/queen-of-hearts.png"},
+
+{rank:"queen",
+suit:"diamonds",
+cardImage:"images/queen-of-diamonds.png"},
+
+{rank:"king",
+suit:"hearts",
+cardImage:"images/king-of-hearts.png"},
+
+{rank:"king",
+suit:"diamonds",
+cardImage:"images/king-of-diamonds.png"}];
+
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -21,7 +37,7 @@ var flipCard = function(cardId){
 		console.log('blank');
 	}
 //end of code pasted 
-console.log("User flipped" + cards[cardId])
+console.log("User flipped" + card[cardId]);
 cardsInPlay.push(cardId);
 checkForMatch();
 };
