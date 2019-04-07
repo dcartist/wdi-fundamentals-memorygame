3e75771b074acd686c9ae9bf334a6630fc59e63b
dcartist <Pbannerman13@gmail.com>
2019-04-07 16:42:39 -0400

    new changes to JS
    it works but second card flipping is off.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,4 @@
-var cards =[
+var cards = [ 
 {rank:"queen",
 suit:"hearts",
 cardImage:"images/queen-of-hearts.png"},
@@ -14,36 +14,129 @@ cardImage:"images/king-of-hearts.png"},
 {rank:"king",
 suit:"diamonds",
 cardImage:"images/king-of-diamonds.png"}];
-
+// var count = 0;
 var cardsInPlay = [];
-
+var returnCard = function(a){
+	var x = document.getElementById(a);
+	x.setAttribute('src','images/back.png');
+	return;
+	
+};
 var checkForMatch = function(){
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	  console.log("You found a match!");
+	if(cardsInPlay.length == 2){
+		if (cards[cardsInPlay[0]].suit == cards[cardsInPlay[1]].suit) {
+		  console.log("You found a match!");
+		  // var x = docuement.get
+		  console.log(cardsInPlay[0] + cardsInPlay[1])
+		  // var x = document.getElementById(cardsInPlay[0]);
+		  // var x = document.getElementById(cardsInPlay[0]).src;
+		  // console.log('Data by id '+ x);
+		  // this.setAttribute('src',cards[i].cardImage);
+		  // var checking = this.getAttribute('data-id');
+		  alert('you found a match!');
+		  // setTimeout(checkForMatch, 5000);
+		  // window.setTimeout(window.alert, 1000, 'That was really slow!');
+		  returnCard(cardsInPlay[0]);
+		  returnCard(cardsInPlay[1]);
+		  cardsInPlay.length = 0;
+
+		  // x.setAttribute('src','images/back.png');
+
+		} else {
+		  console.log("Sorry, try again.");
+		  alert('Sorry!');
+		  returnCard(cardsInPlay[0]);
+		  returnCard(cardsInPlay[1]);
+		  cardsInPlay.length = 0;
+		};
 	} else {
-	  console.log("Sorry, try again.");
-	}
+		console.log('one more');
+
+		// returnCard();
+	};
+	
+};
+var turnOver = function(){
+	// this.setAttribute('src','images/back.png');
+	var checking = this.getAttribute('data-id');
+	this.setAttribute('src',cards[checking].cardImage);
+	this.setAttribute('data-check',true);
+	this.setAttribute('alt',true);
+	cardsInPlay.push(checking);
+	// cardsInPlay.push(cards[checking].suit);
+
+	console.log('turned'+ count + " "+ checking + " "+ cards[checking].suit);
+	checkForMatch();
 }
 
-var flipCard = function(cardId){
-//beginning of code pasted
-	if (cardsInPlay.length == 2){
-		if (cardsInPlay[0] == cardsInPlay[1]){
-			alert("You found a match!");
-		} else {
-			alert("Sorry, try again.");
-		}
-	} else {
-		console.log('blank');
-	}
-//end of code pasted 
-console.log("User flipped" + card[cardId]);
-cardsInPlay.push(cardId);
-checkForMatch();
-};
 
-flipCard(0);
-flipCard(2);
+var cardElement = [];
+
+var createBoard = function() {
+	// body... 
+	for (i=0;i<4;i++){
+	var newCardItem = document.createElement('img');
+	// newCardItem.setAttribute('src', cards[i].cardImage);
+	newCardItem.setAttribute('style', 'margin: 40px 10px;');
+	newCardItem.setAttribute('data-id',i);
+	newCardItem.setAttribute('data-check',false);
+	// newCardItem.setAttribute('alt',false);
+	newCardItem.setAttribute('id', i);
+	newCardItem.setAttribute('src', 'images/back.png');
+	newCardItem.addEventListener('click', turnOver);
+	cardElement.push(newCardItem);
+	document.getElementById('game-board').appendChild(newCardItem);
+	}
+};
+  createBoard();
+
+
+
+// var createBoad = function() {
+// 	for (i=0;i<4;i++){
+// 	var newCardItem = document.createElement('img');
+// 	// newCardItem.setAttribute('src', cards[i].cardImage);
+	
+// 	newCardItem.setAttribute('src', 'images/back.png');
+// 	newCardItem.setAttribute('style', 'margin: 40px 10px;');
+// 	// newCardItem.classList.add = "cards";
+// 	// newCardItem.addEventListener('click',turnOver);
+// 	document.getElementById('game-board').appendChild(newCardItem);
+	
+// 	};
+
+
+
+// }
+// var checkForMatch = function(){
+// 	if (cardsInPlay[0] === cardsInPlay[1]) {
+// 	  console.log("You found a match!");
+// 	} else {
+// 	  console.log("Sorry, try again.");
+// 	}
+// }
+
+// var flipCard = function(cardId){
+// //beginning of code pasted
+// 	if (cardsInPlay.length == 2){
+// 		if (cardsInPlay[0] == cardsInPlay[1]){
+// 			alert("You found a match!");
+// 		} else {
+// 			alert("Sorry, try again.");
+// 		}
+// 	} else {
+// 		console.log('blank');
+// 	}
+// //end of code pasted 
+// console.log("User flipped" + card[cardId]);
+// cardsInPlay.push(cardId);
+// checkForMatch();
+// };
+
+// flipCard(0);
+// flipCard(2);
+
+
 
 
 
