0403add5a8c13bb7b7aa4fe3c27f8aa8042f5257
dcartist <Pbannerman13@gmail.com>
2019-04-07 17:17:09 -0400

    no reset on it.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -15,64 +15,61 @@ cardImage:"images/king-of-hearts.png"},
 suit:"diamonds",
 cardImage:"images/king-of-diamonds.png"}];
 // var count = 0;
-var cardsInPlay = [];
+var cardsInPlay = []; // shows what cards are in play
+
+//flipping the card back over to back.png 
 var returnCard = function(a){
 	var x = document.getElementById(a);
 	x.setAttribute('src','images/back.png');
 	return;
-	
 };
+//reseting the cards in play
+var reset = function(){
+cardsInPlay.length = 0;
+};
+
 var checkForMatch = function(){
 	if(cardsInPlay.length == 2){
 		if (cards[cardsInPlay[0]].suit == cards[cardsInPlay[1]].suit) {
 		  console.log("You found a match!");
 		  console.log(cardsInPlay[0] + cardsInPlay[1])
-		  // var x = document.getElementById(cardsInPlay[0]);
-		  // var x = document.getElementById(cardsInPlay[0]).src;
-		  // console.log('Data by id '+ x);
-		  // this.setAttribute('src',cards[i].cardImage);
-		  // var checking = this.getAttribute('data-id');
 		  alert('you found a match!');
-		  // setTimeout(checkForMatch, 5000);
-		  // window.setTimeout(window.alert, 1000, 'That was really slow!');
-		  returnCard(cardsInPlay[0]);
-		  returnCard(cardsInPlay[1]);
-		  cardsInPlay.length = 0;
-
-		  // x.setAttribute('src','images/back.png');
+		  // returnCard(cardsInPlay[0]);
+		  // returnCard(cardsInPlay[1]);
+		  reset();
 
 		} else {
 		  console.log("Sorry, try again.");
 		  alert('Sorry!');
-		  returnCard(cardsInPlay[0]);
-		  returnCard(cardsInPlay[1]);
-		  cardsInPlay.length = 0;
+		  // returnCard(cardsInPlay[0]);
+		  // returnCard(cardsInPlay[1]);
+		  reset();
 		};
 	} else {
 		console.log('one more');
-
-		// returnCard();
 	};
 	
 };
+
+//turnig the card 
 var turnOver = function(){
 	// this.setAttribute('src','images/back.png');
 	var checking = this.getAttribute('data-id');
 	this.setAttribute('src',cards[checking].cardImage);
 	this.setAttribute('data-check',true);
 	this.setAttribute('alt',true);
+	console.log('turned'+ checking + " "+ cards[checking].suit);
+	
 	cardsInPlay.push(checking);
-	// cardsInPlay.push(cards[checking].suit);
 
-	console.log('turned'+ count + " "+ checking + " "+ cards[checking].suit);
 	checkForMatch();
-}
+	// cardsInPlay.push(cards[checking].suit);
+};
 
 
 var cardElement = [];
 
 var createBoard = function() {
-	// body... 
 	for (i=0;i<4;i++){
 	var newCardItem = document.createElement('img');
 	newCardItem.setAttribute('style', 'margin: 40px 10px;');
